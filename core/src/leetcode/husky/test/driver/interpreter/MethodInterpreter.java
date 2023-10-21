23d143615254149b0e87e0aa25860fea182e8079
runnableAir <1316214297@qq.com>
2023-10-21 20:15:14 +0800

    fix: 修改MethodInterpreter的注释
    MethodInterpreter的getMethodArgs
    CommonMethodInterpreter的getMethodArgs的注释

--- core/src/leetcode/husky/test/driver/interpreter/MethodInterpreter.java
+++ core/src/leetcode/husky/test/driver/interpreter/MethodInterpreter.java
@@ -39,15 +39,16 @@ public abstract class MethodInterpreter<T> {
     protected abstract MethodProxy<T> getMethodProxy(Command command);
 
     /**
-     * Return an array of Object type representing the arguments converted
-     * from the command arguments for the target method specified by the
-     * command.
+     * Return an array of the arguments that are conveyed to the target
+     * method specified by the {@linkplain Command} object.
      * <p>
-     * It takes the command parameters as input and converts them into
-     * arguments that match the arguments list of the target method.
+     * This method takes the arguments from the {@code Command} object
+     * as input and converts them into arguments that match the target
+     * method's argument list.
      *
      * @param command command
-     * @return an array of Object type, representing the results
+     * @return an array of objects representing the arguments for the
+     * target method
      */
     protected abstract Object[] getMethodArgs(Command command);
 }
