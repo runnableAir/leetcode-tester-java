7c3083b1d576ed18884e83dca535750a957d51ef
runnableAir <1316214297@qq.com>
2023-11-12 00:23:03 +0800

    refactor(util): 字符串扫描替代正则避免回溯死机

--- core/src/leetcode/husky/test/util/ArgumentParseUtil.java
+++ core/src/leetcode/husky/test/util/ArgumentParseUtil.java
@@ -1,36 +1,25 @@
 package leetcode.husky.test.util;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Function;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class ArgumentParseUtil {
-    static final String NUM_REGEX = "-?(?!0\\d)\\d+";
-    static final String ARRAY_ITEM_REGEX = "(\"((?:\\\\\"|[^\"])*)\"|(" + NUM_REGEX + "))";
 
     public static List<String> getStringList(String arrayLiked) {
-        checkArrayLiked(ARRAY_ITEM_REGEX, arrayLiked, "string");
-        return getList(String.class, arrayLiked);
+        return getList(arrayLiked, STRING_TYPE);
     }
 
     public static List<List<String>> getString2dList(String arrayLiked) {
-        String itemRegex = makeArrayLikedRegex(ARRAY_ITEM_REGEX);
-        checkArrayLiked(itemRegex, arrayLiked, "2d-string");
-        return get2dList(String.class, arrayLiked);
+        return get2dList(arrayLiked, STRING_TYPE);
     }
 
     public static List<Integer> getIntList(String arrayLiked) {
-        checkArrayLiked(NUM_REGEX, arrayLiked, "int");
-        return getList(Integer.class, arrayLiked);
+        return getList(arrayLiked, INT_TYPE);
     }
 
     public static List<List<Integer>> getInt2dList(String arrayLiked) {
-        String itemRegex = makeArrayLikedRegex(NUM_REGEX);
-        checkArrayLiked(itemRegex, arrayLiked, "2d-int");
-        return get2dList(Integer.class, arrayLiked);
+        return get2dList(arrayLiked, INT_TYPE);
     }
 
     public static String[] getStringArray(String arrayLiked) {
@@ -60,61 +49,256 @@ public class ArgumentParseUtil {
                 .toArray(int[][]::new);
     }
 
-    static <T> List<T> getList(Class<T> elementType, String arrayLiked) {
-        Objects.requireNonNull(elementType);
-        Function<String, T> transformer = getTransformer(elementType);
-        return splitArrayLiked(arrayLiked)
-                .map(transformer)
+    static <T> List<T> getList(String src, int elementType) {
+        TreeNode treeNode = buildTree(src, 1, elementType);
+        Function<LeafNode<?>, T> mapper = getLeafMapper(elementType);
+        return getListFromTreeNode(treeNode, mapper);
+    }
+
+    static <T> List<List<T>> get2dList(String src, int elementType) {
+        TreeNode treeNode = buildTree(src, 2, elementType);
+        Function<LeafNode<?>, T> mapper = getLeafMapper(elementType);
+        return treeNode.getChildren()
+                .stream()
+                .map(node -> getListFromTreeNode(node, mapper))
                 .toList();
     }
 
-    static <T> List<List<T>> get2dList(Class<T> elementType, String _2dArrayLiked) {
-        return splitArrayLiked(_2dArrayLiked)
-                .map(a -> getList(elementType, a))
-                .toList();
-    }
-
-    static void checkArrayLiked(String itemRegex, String arrayLiked, String expectedElementType) {
-        if (arrayLiked == null) {
-            throw new IllegalArgumentException("\"arrayLiked\" can not be null");
-        }
-        if (itemRegex == null) {
-            throw new IllegalStateException("\"itemRegex\" can not be null");
-        }
-        String regex = makeArrayLikedRegex(itemRegex);
-        if (!arrayLiked.matches(regex)) {
-            throw new IllegalArgumentException(
-                    "\"" + arrayLiked + "\" is a invalid string for " + expectedElementType + " array");
+    static <T> List<T> getListFromTreeNode(TreeNode node, Function<LeafNode<?>, T> leafMapper) {
+        List<TreeNode> children = node.getChildren();
+        try {
+            return children.stream()
+                    .map(treeNodeCastToLeafNodeFunc())
+                    .map(leafMapper)
+                    .toList();
+        } catch (IllegalStateException e) {
+            throw new IllegalStateException("all child must be an instance of LeafNode<?>", e);
         }
     }
 
-    /**
-     * Return a regular expression of an array formed by the regular
-     * expression of its elements.
-     *
-     * @param element the regular expression of the element
-     * @return a regular expression of an array
-     */
-    static String makeArrayLikedRegex(String element) {
-        return "\\[((" + element + ")?|(" + element + "(,\\s*" + element + ")+))]";
+    static Function<TreeNode, LeafNode<?>> treeNodeCastToLeafNodeFunc() {
+        return node -> {
+            Objects.requireNonNull(node);
+            if (node instanceof LeafNode<?> leaf) {
+                return leaf;
+            }
+            throw new IllegalStateException("node is not a leaf node: " + node);
+        };
     }
 
-    private static Stream<String> splitArrayLiked(String arrayLiked) {
-        int prefix = 0;
-        int length = arrayLiked.length();
-        while (prefix < length) {
-            if (arrayLiked.charAt(prefix) != '[') break;
-            prefix++;
+    static Function<LeafNode<?>, String> getStringNodeValueFunc() {
+        return leafNode -> {
+            Objects.requireNonNull(leafNode);
+            Object val = leafNode.getVal();
+            return val.toString();
+        };
+    }
+
+    static Function<LeafNode<?>, Number> getNumberNodeValueFunc() {
+        return leafNode -> {
+            Objects.requireNonNull(leafNode);
+            if (leafNode instanceof NumberNode node) {
+                return node.getVal();
+            }
+            throw new IllegalStateException();
+        };
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> Function<LeafNode<?>, T> getLeafMapper(int elementType) {
+        return switch (elementType) {
+            case INT_TYPE -> (Function<LeafNode<?>, T>) getNumberNodeValueFunc().andThen(Number::intValue);
+            case INT64_TYPE -> (Function<LeafNode<?>, T>) getNumberNodeValueFunc().andThen(Number::longValue);
+            case STRING_TYPE -> (Function<LeafNode<?>, T>) getStringNodeValueFunc().andThen(ArgumentParseUtil::removeRedundantQuote);
+            default -> throw new IllegalStateException("Unexpected value: " + elementType);
+        };
+    }
+
+    public static final int INT_TYPE = 0x1;
+    public static final int INT64_TYPE = 0x2;
+    public static final int NUMBER_TYPE = 0x3;
+    public static final int STRING_TYPE = 0x4;
+
+    private static void arrayLikedValidate(String arrayLiked, int dimension) {
+        if (dimension == 0) {
+            throw new IllegalArgumentException("the dimension of the array can not be zero");
         }
-        int innerBraceCnt = prefix - 1;
-        arrayLiked = arrayLiked.substring(prefix, length - prefix);
         if (arrayLiked.isEmpty()) {
-            return Stream.empty();
+            throw new IllegalArgumentException("can not convert the string as an array because it is empty");
         }
-        String[] split = arrayLiked.split("]{%d},\\s*\\[{%1$d}".formatted(innerBraceCnt));
-        String left = "[".repeat(innerBraceCnt);
-        String right = "]".repeat(innerBraceCnt);
-        return Arrays.stream(split).map(item -> left + item + right);
+        int len = arrayLiked.length();
+        if (arrayLiked.charAt(0) != '[' || arrayLiked.charAt(len - 1) != ']') {
+            throw new IllegalArgumentException(
+                    "can not convert the string as an array because it isn't wrapped in \"[]\""
+            );
+        }
+    }
+
+    private static TreeNode buildTree(String arrayLiked, int dimension, int allowElementType) {
+        arrayLikedValidate(arrayLiked = arrayLiked.strip(), dimension);
+        int len = arrayLiked.length();
+        String s = arrayLiked;
+        Deque<TreeNode> stk = new ArrayDeque<>();
+        TreeNode root = new TreeNode();
+        TreeNode cur = root;
+        stk.push(root);
+        int curDimension = 1;
+        int i = 1;
+        // each loop we scan and make a node
+        while (i < len && curDimension > 0 && curDimension <= dimension) {
+            char c;
+            // skip white spaces
+            if (Character.isWhitespace(c = s.charAt(i))) {
+                i++;
+                continue;
+            }
+            // into a new array (push node)
+            if (c == '[') {
+                curDimension++;
+                // new child node
+                TreeNode child = new TreeNode();
+                cur.appendChild(child);
+                cur = child;
+                stk.push(child);
+                i++;
+                continue;
+            }
+
+            /*
+             * handle string or number leaf node if the array
+             * IS NOT empty.
+             *
+             * if the array IS NOT empty:
+             * (1) we already scanned some leaf nodes of current node
+             * (2) or we scanned no leaf node, and we don't meet the
+             *     end char(']') which means no leaf node found
+             */
+            if (!cur.getChildren().isEmpty() || c != ']') {
+                // ready to handle a leaf node but check
+                // if current dimension is enough at first
+                if (curDimension < dimension) {
+                    throw new IllegalArgumentException(
+                            "expected '[' but found '%s' at %d pos, because the array should be %d dimension"
+                                    .formatted(c, i, dimension)
+                    );
+                }
+                i = scanOneLeafChild(i, s, cur, allowElementType);
+            }
+
+            // skip white spaces
+            while (i < len && Character.isWhitespace(c = s.charAt(i))) {
+                i++;
+            }
+            // expected ',' or ']'
+            if (i == len) {
+                throw new IllegalArgumentException("expected ',' or ']' but noting found");
+            }
+            if (c != ',' && c != ']') {
+                throw new IllegalArgumentException("expected ',' or ']' at %d pos but found '%s'".formatted(i, c));
+            }
+
+            // handle all end char(']') if we meet
+            // and go to next sep char(',')
+            while (i < len && curDimension > 0 &&  c == ']') {
+                curDimension--;
+                i++;
+                stk.pop();
+                // cur = stk.peek();
+                if (!stk.isEmpty()) {
+                    // actually we don't need to check whether stack
+                    // is empty, here is for IDE code checking for NPE
+                    cur = stk.peek();
+                }
+                // skip white spaces
+                while (i < len && Character.isWhitespace(c = s.charAt(i))) {
+                    i++;
+                }
+            }
+            if (i == len) {
+                break;
+            }
+            // expected ','
+            if (c != ',') {
+                throw new IllegalArgumentException("expected ',' at %d pos but found '%s'".formatted(i, c));
+            }
+            i++;
+        }
+
+        if (curDimension > dimension) {
+            throw new IllegalArgumentException("too many '[' at %d pos that make array dimension > limit".formatted(i));
+        }
+        if (curDimension > 0) {
+            throw new IllegalArgumentException("expected ']' but the array is end");
+        }
+        if (i < len) {
+            throw new IllegalArgumentException("the array is end but found '%s'...".formatted(s.charAt(i)));
+        }
+        return root;
+    }
+
+    private static int scanOneLeafChild(int begin, String s, TreeNode parent, int allowElementType) {
+        int i = begin;
+        char c = s.charAt(i);
+        if (c == '"') {
+            if ((allowElementType & STRING_TYPE) == 0) {
+                throw new IllegalArgumentException("the \"string\" element is not allowed");
+            }
+            String val = s.substring(i, (i = scanString(i, s)));
+            parent.appendChild(new StringNode(val));
+        } else if (c == '0' || c == '-' || ('1' <= c && c <= '9')) {
+            if ((allowElementType & NUMBER_TYPE) == 0) {
+                throw new IllegalArgumentException("the \"number\" element is not allowed");
+            }
+            String val = s.substring(i, (i = scanNumber(i, s)));
+            parent.appendChild(new NumberNode(val));
+        } else {
+            throw new IllegalArgumentException(
+                    "expected '\"' or '-' or '0'~'9' at %d pos but found '%s'".formatted(i, c)
+            );
+        }
+        return i;
+    }
+
+    static int scanNumber(int being, String s) {
+        int i = being, len = s.length();
+        char c = s.charAt(i);
+        if (c == '-' && i < len - 1) {
+            c = s.charAt(++i);
+        }
+        if (!Character.isDigit(c)) {
+            throw new IllegalArgumentException("expected '0'~'9' at %d pos but found '%s'".formatted(i, c));
+        }
+        if (c == '0' && i < len - 1 && Character.isDigit(s.charAt(++i))) {
+            throw new IllegalArgumentException("expected '1'~'9' at %d pos but found 0".formatted(i));
+        }
+        while (i < len && Character.isDigit(s.charAt(i))) {
+            i++;
+        }
+        return i;
+    }
+
+    static int scanString(int begin, String s) {
+        int i = begin + 1, len = s.length();
+        while (i < len) {
+            while (i < len && s.charAt(i) != '"') {
+                i++;
+            }
+            if (s.charAt(i - 1) != '\\') {
+                break;
+            }
+            i++;
+        }
+        if (i == len) {
+            throw new IllegalArgumentException(
+                    "expected '%s' at %d pos when handling the string(%s) beginning at '%d' pos"
+                            .formatted('"', i, s.substring(begin, i), begin)
+            );
+        }
+        return i + 1;
+    }
+
+    static String checkArrayLiked(String arrayLiked, int dimension, int allowElementType) {
+        return buildTree(arrayLiked, dimension, allowElementType).toString();
     }
 
     public static String removeRedundantQuote(String s) {
@@ -127,15 +311,74 @@ public class ArgumentParseUtil {
         return s;
     }
 
-    @SuppressWarnings("unchecked")
-    static <T> Function<String, T> getTransformer(Class<T> elementType) {
-        String simpleName = elementType.getSimpleName();
-        // TODO add more enhance condition
-        if ("String".equals(simpleName)) {
-            return str -> (T) removeRedundantQuote(str);
-        } else if ("Integer".equals(simpleName)) {
-            return str -> (T) Integer.valueOf(str);
+}
+
+class TreeNode {
+    private final List<TreeNode> children = new ArrayList<>();
+
+    public List<TreeNode> getChildren() {
+        return children;
+    }
+
+    void appendChild(TreeNode treeNode) {
+        children.add(treeNode);
+    }
+
+    @Override
+    public String toString() {
+        return children.toString();
+    }
+}
+
+abstract class LeafNode<T> extends TreeNode {
+    private final T val;
+
+    LeafNode(String val) {
+        this.val = parse(val);
+    }
+
+    protected abstract T parse(String val);
+
+    public T getVal() {
+        return val;
+    }
+}
+
+class NumberNode extends LeafNode<Number> {
+
+    NumberNode(String val) {
+        super(val);
+    }
+
+    @Override
+    protected Number parse(String val) {
+        return Long.valueOf(val);
+    }
+
+    @Override
+    public String toString() {
+        return getVal().toString() + "(Number)";
+    }
+}
+
+class StringNode extends LeafNode<String> {
+
+    StringNode(String val) {
+        super(val);
+    }
+
+    @Override
+    protected String parse(String val) {
+        int len = val.length();
+        if (val.charAt(0) != '"' && val.charAt(len - 1) != '"') {
+            throw new IllegalStateException("can not parse '%s' to string val because it isn't wrapped by '\"'");
         }
-        throw new IllegalStateException("unsupported element type : \"" + elementType + "\"");
+        val = val.substring(1, len - 1);
+        return val.replaceAll("\\\\\"", "\"");
+    }
+
+    @Override
+    public String toString() {
+        return getVal() + "(String)";
     }
 }
---
+++ core/test/leetcode/husky/test/util/ArgumentParseUtilTest.java
@@ -0,0 +1,270 @@
+package leetcode.husky.test.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class ArgumentParseUtilTest {
+
+    @Test
+    public void testInvalidIntList() {
+        List<String> invalidStrList = List.of(
+                "[,1,]",
+                "[1,2,]",
+                "[1,2,a]",
+                "[\"1\",\"2\"]",
+                "[0,-1,--1]",
+                "[0,-1,001]"
+        );
+        invalidStrList.forEach(testerOfInvalidArrayParsing(ArgumentParseUtil::getIntList));
+    }
+
+    @Test
+    public void testInvalidInt2dList() {
+        List<String> invalidStrList = List.of(
+                "[[1,2],3]",
+                "[1,[2,3]]",
+                "[[1,2]",
+                "[1,2]]",
+                "",
+                "[[,]]",
+                "[[1,]]",
+                "[[,1]]",
+                "[[,1,]]",
+                "[[][]]",
+                "[[1,2],[]",
+                "[[-1,2],[+1]]",
+                "[[-1,2],[-+1]]"
+        );
+        invalidStrList.forEach(testerOfInvalidArrayParsing(ArgumentParseUtil::getInt2dList));
+    }
+
+    @Test
+    public void testInvalidStringList() {
+        List<String> invalidStrList = List.of(
+                "[\"test]",
+                "[test\"]",
+                "[test]",
+                "[\"test\",]",
+                "[\"test\"\"]",
+                "[\"\",\"]",
+                "[\"\",\"\"\"]",
+                "[\"a\",]",
+                "[,\"a\"]",
+                "[,]",
+                "[[a]]",
+                "[\"a\",'c\"]",
+                "[\"a\"\"b\"]",
+                "[1,\"2]"
+        );
+        invalidStrList.forEach(testerOfInvalidArrayParsing(ArgumentParseUtil::getStringList));
+    }
+
+    @Test
+    public void testInvalidString2dList() {
+        List<String> invalidStrList = List.of(
+                "[[][]]",
+                "[[],[]",
+                "[[\"hello\",\"world\"][]]",
+                "[[] [\"hello\",\"world\"]]",
+                "[[\"1\", \"2], [\"hello\",\"world\"]]",
+                "[[\"hello->\"world\"\"]]"
+        );
+        invalidStrList.forEach(testerOfInvalidArrayParsing(ArgumentParseUtil::getString2dList));
+    }
+
+    @Test
+    public void testStringList() {
+        List<String> strList = List.of(
+                "[\"ab\", \"b\"]",
+                "[\"print\", \"(\\\"\", \"hello world!\", \"\\\")\"]"
+        );
+        List<List<String>> expectedResults = List.of(
+                List.of("ab", "b"),
+                List.of("print", "(\"", "hello world!", "\")")
+        );
+        for (int i = 0; i < strList.size(); i++) {
+            String str = strList.get(i);
+            List<String> list = ArgumentParseUtil.getStringList(str);
+            List<String> expected = expectedResults.get(i);
+            Assert.assertEquals(expected, list);
+        }
+    }
+
+    @Test
+    public void testString2dList() {
+        var strList = List.of(
+                "[[\"a\", \"ba\"],[\"ad\", \"b\"]]",
+                // [["a", "ba"],["ad", "b"],["print", "(\"", "hello world!", "\")"]]
+                "[[\"a\", \"ba\"],[\"ad\", \"b\"],[\"print\", \"(\\\"\", \"hello world!\", \"\\\")\"]]"
+        );
+        var expectedList = List.of(
+                List.of(List.of("a", "ba"), List.of("ad", "b")),
+                List.of(List.of("a", "ba"), List.of("ad", "b"), List.of("print", "(\"", "hello world!", "\")"))
+        );
+        strList.forEach(testerOfArrayParsing(expectedList, ArgumentParseUtil::getString2dList));
+    }
+
+    @Test
+    public void testIntArrayLiked() {
+        var strList = List.of(
+                "[1]",
+                "[1,2,12]",
+                "[1, 2,  12]",
+                "[-1,-2,-3]"
+        );
+
+        // toIntList
+        var expectedList = List.of(
+                List.of(1),
+                List.of(1, 2, 12),
+                List.of(1, 2, 12),
+                List.of(-1, -2, -3)
+        );
+        strList.forEach(testerOfArrayParsing(expectedList, ArgumentParseUtil::getIntList));
+
+        // toIntArray
+        var expectedList2 = expectedList.stream()
+                .map(l -> l.stream().mapToInt(Integer::intValue).toArray())
+                .toList();
+        strList.forEach(testerOfArrayParsing(
+                expectedList2,
+                ArgumentParseUtil::getIntArray,
+                Assert::assertArrayEquals)
+        );
+    }
+
+    @Test
+    public void testInt2dList() {
+        var strList = List.of(
+                "[[1,2],[3,4]]",
+                "[[4,2,1,2]]",
+                "[[-1,-2,-3]]",
+                "[[-1,-2],[-3]]",
+                "[[-1],[-2,-3]]"
+        );
+        var expectedList = List.of(
+                List.of(List.of(1, 2), List.of(3, 4)),
+                List.of(List.of(4, 2, 1, 2)),
+                List.of(List.of(-1, -2, -3)),
+                List.of(List.of(-1, -2), List.of(-3)),
+                List.of(List.of(-1), List.of(-2, -3))
+        );
+        strList.forEach(testerOfArrayParsing(expectedList, ArgumentParseUtil::getInt2dList));
+    }
+
+    @Test
+    public void testStringArray() {
+        var strList = List.of(
+                "[\"ab\", \"b\"]",
+                "[\"print\", \"(\\\"\", \"hello world!\", \"\\\")\"]"
+        );
+        var expectedList = List.of(
+                new String[]{"ab", "b"},
+                new String[]{"print", "(\"", "hello world!", "\")"}
+        );
+        strList.forEach(testerOfArrayParsing(expectedList, ArgumentParseUtil::getStringArray, Assert::assertArrayEquals));
+    }
+
+    @Test
+    public void testString2dArray() {
+        var strList = List.of(
+                "[[\"a\", \"ba\"],[\"ad\", \"b\"]]",
+                // [["a", "ba"],["ad", "b"],["print", "(\"", "hello world!", "\")"]]
+                "[[\"a\", \"ba\"],[\"ad\", \"b\"],[\"print\", \"(\\\"\", \"hello world!\", \"\\\")\"]]"
+        );
+        var expectedList = List.of(
+                new String[][]{{"a", "ba"}, {"ad", "b"}},
+                new String[][]{{"a", "ba"}, {"ad", "b"}, {"print", "(\"", "hello world!", "\")"}}
+        );
+        strList.forEach(testerOfArrayParsing(expectedList, ArgumentParseUtil::getString2dArray, Assert::assertArrayEquals));
+    }
+
+    @Test
+    public void testIntArray() {
+        var str = "[1,17]";
+        int[] array = ArgumentParseUtil.getIntArray(str);
+        int[] expected = {1, 17};
+        Assert.assertArrayEquals(expected, array);
+    }
+
+    @Test
+    public void testInt2dArray() {
+        var str = "[[1,1],[1,11]]";
+        int[][] list = ArgumentParseUtil.getInt2dArray(str);
+        int[][] expected = {
+                {1, 1},
+                {1, 11}
+        };
+        Assert.assertArrayEquals(expected, list);
+    }
+
+    @Test
+    public void testCheckArrayOfAnyArray() {
+        List<String> anyArray = List.of(
+                "[\"abc\", \"abc\"]",
+                "[\"abc\", \"abc\", 1]",
+                "[]"
+        );
+        anyArray.forEach(s -> System.out.println(ArgumentParseUtil.checkArrayLiked(s, 1, ArgumentParseUtil.NUMBER_TYPE | ArgumentParseUtil.STRING_TYPE)));
+    }
+
+    @Test
+    public void testCheckArrayOfAny2dArray() {
+        List<String> any2dArray = List.of(
+                "[[\"abc\", \"abc\"],[123,1234556789,-987]]",
+                "[]"
+        );
+        any2dArray.forEach(s -> System.out.println(ArgumentParseUtil.checkArrayLiked(s, 2, ArgumentParseUtil.NUMBER_TYPE | ArgumentParseUtil.STRING_TYPE)));
+    }
+
+    @Test
+    public void testCheckArrayOfIntArray() {
+        List<String> intArray = List.of(
+                "[123,  -233\n\t, 3]",
+                "[1,2,-1,2,0]",
+                "[]"
+        );
+        intArray.forEach(s -> System.out.println(ArgumentParseUtil.checkArrayLiked(s, 1, ArgumentParseUtil.NUMBER_TYPE)));
+    }
+
+    @Test
+    public void testCheckArrayOfInt2dArray() {
+        List<String> int2dArray = List.of(
+                "[ [123  , -233 ] ,   [3 ] ]",
+                "[[123 , -233] ]",
+                "[]"
+        );
+        int2dArray.forEach(s -> System.out.println(ArgumentParseUtil.checkArrayLiked(s, 2, ArgumentParseUtil.NUMBER_TYPE)));
+    }
+
+    private static Consumer<String> testerOfInvalidArrayParsing(Consumer<String> arrayLikedParser) {
+        return str -> {
+            System.out.println(str);
+            var exception = Assert.assertThrows(
+                    "%s should be invalid!".formatted(str),
+                    IllegalArgumentException.class,
+                    () -> arrayLikedParser.accept(str)
+            );
+            String message = exception.getMessage();
+            System.err.println(message);
+        };
+    }
+
+    private static <T> Consumer<String> testerOfArrayParsing(List<T> expectedResults, Function<String, T> arrayLikedParser) {
+        return testerOfArrayParsing(expectedResults, arrayLikedParser, Assert::assertEquals);
+    }
+
+    private static <T> Consumer<String> testerOfArrayParsing(List<T> expectedResults, Function<String, T> arrayLikedParser, BiConsumer<T, T> doAssert) {
+        var it = expectedResults.iterator();
+        return str -> {
+            T actual = arrayLikedParser.apply(str);
+            T expected = it.next();
+            doAssert.accept(expected, actual);
+        };
+    }
+}
