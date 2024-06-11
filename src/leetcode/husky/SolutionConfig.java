36146bea5737d0daa9444816ef854bad5e136e9d
runnableAir <1316214297@qq.com>
2024-06-11 15:34:24 +0800

    add CommandReader for SolutionTester
    MainInvokeCommandReader(default mode):
    - read `n` lines to invoke single target method
    MultiInvokeCommandReader:
    - read 2 lines of a sequence of invoke methods and its parameter lists

--- src/leetcode/husky/SolutionConfig.java
+++ src/leetcode/husky/SolutionConfig.java
@@ -1,19 +1,25 @@
 package leetcode.husky;
 
+import leetcode.husky.test.cmd.Command;
+import leetcode.husky.test.cmd.CommandSet;
+import leetcode.husky.test.cmd.reader.CommandReader;
+import leetcode.husky.test.cmd.reader.LineReader;
 import leetcode.husky.test.driver.v2.InitialInvocation;
 import leetcode.husky.test.driver.v2.InitialTargetHandler;
 import leetcode.husky.test.driver.v2.MethodInvocation;
 import leetcode.husky.test.driver.v2.MethodInvokeHandler;
 import leetcode.husky.test.driver.v2.MethodInvokeRequest;
+import util.husky.array.ArrayStringUtil;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 // Provide simple way to create MethodInvokeHandlers for MethodInvocation and InitialInvocation
 // to config available method of a solution object
 public class SolutionConfig<T> {
-    Map<String, MethodInvokeHandler<T>> handlerMap = new HashMap<>();
+    private final Map<String, MethodInvokeHandler<T>> handlerMap = new HashMap<>();
+    private static final String DEFAULT_METHOD_KEY = "default.method";
+    private static final String DEFAULT_CONSTRUCTOR_KEY = "default.constructor";
+    private int defaultMethodArgument = -1;
 
 
     public void addMethod(String key,
@@ -21,6 +27,11 @@ public class SolutionConfig<T> {
                           StringConverter... stringConverters) {
         var handler = new SimpleMethodInvokeHandler<>(methodInvocation, List.of(stringConverters));
         handlerMap.put(key, handler);
+        // try to set "default" method mapping (if absent)
+        if (defaultMethodArgument == -1) {
+            handlerMap.put(DEFAULT_METHOD_KEY, handler);
+            defaultMethodArgument = stringConverters.length;
+        }
     }
 
     public void addConstructor(String key,
@@ -28,6 +39,23 @@ public class SolutionConfig<T> {
                                StringConverter... stringConverters) {
         var handler = new SimpleInitialTargetHandler<>(initialInvocation, List.of(stringConverters));
         handlerMap.put(key, handler);
+        // try to set "default" constructor mapping (if absent)
+        // "default" constructor should require no arguments/converters
+        if (handlerMap.containsKey(DEFAULT_CONSTRUCTOR_KEY) || stringConverters.length > 0) {
+            return;
+        }
+        handlerMap.put(DEFAULT_CONSTRUCTOR_KEY, handler);
+    }
+
+
+    public enum Mode {
+        MAIN_INVOKE, MULTI_INVOKE
+    }
+
+    private Mode mode = Mode.MAIN_INVOKE;
+
+    public void setMode(Mode mode) {
+        this.mode = mode;
     }
 
     void applyTo(SolutionTester<T> solutionTester) {
@@ -36,6 +64,14 @@ public class SolutionConfig<T> {
             MethodInvokeHandler<T> handler = invokeHandlerEntry.getValue();
             solutionTester.addMethodInvokeHandler(targetMethodKey, handler);
         }
+        // determine which command reader should be used for `solutionTester`
+        CommandReader commandReader;
+        if (mode == Mode.MAIN_INVOKE) {
+            commandReader = new MainInvokeCommandReader();
+        } else {
+            commandReader = new MultiInvokeCommandReader();
+        }
+        solutionTester.setCommandReader(commandReader);
     }
 
 
@@ -80,4 +116,72 @@ public class SolutionConfig<T> {
                     .toArray();
         }
     }
+
+    // read command for single main method invocation
+    // main method is the "default" method from this config
+    // 用于读取仅包含目标方法参数输入的命令, 目标方法为当前配置中的 "默认" 方法
+    // 初始化方法为当前配置中的 "默认" 初始化方法
+    class MainInvokeCommandReader implements CommandReader {
+        final int invokeArgumentCount;
+        final Command initialInvoke = new Command(DEFAULT_CONSTRUCTOR_KEY, List.of());
+
+        MainInvokeCommandReader() {
+            // throw Exception if no available "default" constructor. (we need it to prepend an "initial" command)
+            if (!handlerMap.containsKey(DEFAULT_CONSTRUCTOR_KEY)) {
+                throw new RuntimeException("No available \"no-arguments\" InitialTargetInvokeHandler");
+            }
+            this.invokeArgumentCount = defaultMethodArgument;
+        }
+
+        @Override
+        public CommandSet readCommandSet(LineReader lineReader) {
+            List<String> lines = readNLines(lineReader, invokeArgumentCount);
+            if (lines.isEmpty()) {
+                return EMPTY_COMMAND_SET;
+            }
+            Command mainInvoke = new Command(DEFAULT_METHOD_KEY, lines);
+            return new CommandSet(List.of(initialInvoke, mainInvoke));
+        }
+    }
+
+    // read command for multi invoke context (a sequence of method to invoke after
+    // first initial method(constructor))
+    static class MultiInvokeCommandReader implements CommandReader {
+
+        @Override
+        public CommandSet readCommandSet(LineReader lineReader) {
+            List<String> twoLines = readNLines(lineReader, 2);
+            if (twoLines.isEmpty()) {
+                return EMPTY_COMMAND_SET;
+            }
+            List<String> targetMethodKeys = ArrayStringUtil.getStringList(twoLines.get(0));
+            List<List<String>> invokeParameterList = ArrayStringUtil.parse2dArrayAsList(twoLines.get(1));
+            if (targetMethodKeys.size() != invokeParameterList.size()) {
+                throw new RuntimeException(
+                        "The number of \"parameter list\" doesn't match the number of invoking method");
+            }
+            Iterator<List<String>> it = invokeParameterList.iterator();
+            List<Command> commands = targetMethodKeys .stream()
+                    .map(name -> new Command(name, it.next()))
+                    .toList();
+            return new CommandSet(commands);
+        }
+    }
+
+    static List<String> readNLines(LineReader lineReader, int n) {
+        List<String> lines = new ArrayList<>();
+        while (n-- > 0) {
+            String line = null;
+            while ((line == null || line.isBlank()) && lineReader.hasNextLine()) {
+                line = lineReader.nextLine();
+            }
+            if (line == null || line.isBlank()) {
+                break;
+            }
+            lines.add(line);
+        }
+        return lines.size() == n ? lines : List.of();
+    }
+
+    static final CommandSet EMPTY_COMMAND_SET = new CommandSet(List.of());
 }
--- src/leetcode/husky/SolutionTester.java
+++ src/leetcode/husky/SolutionTester.java
@@ -1,9 +1,7 @@
 package leetcode.husky;
 
-import leetcode.husky.test.cmd.CommandSet;
 import leetcode.husky.test.cmd.CommandShell;
 import leetcode.husky.test.cmd.reader.CommandReader;
-import leetcode.husky.test.cmd.reader.LineReader;
 import leetcode.husky.test.driver.v2.MethodInvokeHandler;
 import leetcode.husky.test.driver.v2.ObjectCommandDriver;
 
@@ -11,14 +9,19 @@ import java.io.Reader;
 
 public class SolutionTester<T> {
     final ObjectCommandDriver<T> objectCommandDriver = new ObjectCommandDriver<>();
+    private CommandReader objectCommandReader;
 
 
     public void addMethodInvokeHandler(String targetMethodKey, MethodInvokeHandler<T> handler) {
         objectCommandDriver.addHandler(targetMethodKey, handler);
     }
 
+    public void setCommandReader(CommandReader commandReader) {
+        this.objectCommandReader = commandReader;
+    }
+
     public void startTest(Reader commandInput) {
-        CommandShell commandShell = new CommandShell(objectCommandDriver, lineReader -> null);
+        CommandShell commandShell = new CommandShell(objectCommandDriver, objectCommandReader);
         commandShell.process(commandInput);
     }
 }
