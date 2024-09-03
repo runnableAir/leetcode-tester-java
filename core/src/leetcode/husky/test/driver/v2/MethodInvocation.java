d03eda08b4352e8eec8c8f6b051227aee13031c6
runnableAir <1316214297@qq.com>
2024-09-03 16:37:11 +0800

    add doc for MethodInvocation

--- core/src/leetcode/husky/test/driver/v2/MethodInvocation.java
+++ core/src/leetcode/husky/test/driver/v2/MethodInvocation.java
@@ -1,5 +1,11 @@
 package leetcode.husky.test.driver.v2;
 
+/**
+ * An object representing a method invocation
+ *
+ * @param <T> the type of class or interface that declares the method
+ *            represented by this object
+ */
 public interface MethodInvocation<T> {
 
     Object invoke(T t, Object... arguments);
