690f6769889f1cbb022dbb6045b25a86377a667a
runnableAir <1316214297@qq.com>
2024-09-03 16:36:55 +0800

    add doc for MethodInvokeHandler

--- core/src/leetcode/husky/test/driver/v2/MethodInvokeHandler.java
+++ core/src/leetcode/husky/test/driver/v2/MethodInvokeHandler.java
@@ -2,6 +2,12 @@ package leetcode.husky.test.driver.v2;
 
 import java.util.Objects;
 
+/**
+ * A handler for processing method invocation requests.
+ *
+ * @param <T> the type of the class or interface that declares the method invoked
+ *            by this handler
+ */
 public class MethodInvokeHandler<T> {
     private final MethodInvocation<T> methodInvocation;
     private final ArgumentResolver argumentResolver;
@@ -13,6 +19,13 @@ public class MethodInvokeHandler<T> {
         this.argumentResolver = argumentResolver;
     }
 
+    /**
+     * Get the arguments from {@code methodInvokeRequest} and pass them to the
+     * method being invoked. Invoke the method and return the result.
+     *
+     * @param methodInvokeRequest method invocation request
+     * @return the result of the method invocation
+     */
     public Object handle(MethodInvokeRequest methodInvokeRequest) {
         Object[] arguments = resolveArguments(methodInvokeRequest);
         T t = getMethodInvokeContext().getTarget();
